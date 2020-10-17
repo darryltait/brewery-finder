0c35eefb744a8d153ec3ee2d1d6e8d05912ed0c7
darrylt <4dtait@gmail.com>
2020-10-17 14:23:07 -0600

    update js to display results

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 
 // Contants & Variables
 
-const BASE_URL = 'https://api.openbrewerydb.org/breweries?by_city='
-// const BASE_URL = 'https://api.openbrewerydb.org/breweries?by_city=milwaukee&brewery_type=micro'
+const BASE_URL = 'https://api.openbrewerydb.org/breweries?by_city=';
 
 let userInput, breweryInfo;
 
@@ -10,33 +9,68 @@ let userInput, breweryInfo;
 
 
 // Cached element references
+const $main = $('main');
 const $name = $('#name');
 const $address = $('#address');
 const $city = $('#city');
+const $type = $('#type');
 const $input = $('#input');
 const $submit = $('#submit');
+$breweries = $('#breweries');
 
 
 // Event listeners
-$submit.on('click', handleClick);
+$submit.on('click', handleGetData);
 
 
 
 // Functions
-function handleClick(event) {
+function handleGetData(event) {
     event.preventDefault();
     userInput = $input.val();
-    console.log(userInput);
     if(!userInput) return;
     $.ajax(BASE_URL + userInput)
     .then(function(data){
+
         breweryInfo = data;
+
         render();
+        $input.val('');
     }, function(error){
         console.log('Error: ', error);
     });
 }
 
+function generateUI() {
+    return breweryInfo.map(function(brewery){
+        return `
+            <article class="location">    
+            <h3 id="name">${brewery.name}</h3>
+            <p id="address">${brewery.street}</p>
+            <p id="city">${brewery.city}</p>
+            </article>
+            `;
+        });
+    }
+        
+
+        
+            //<h3 id="name">${brewery.name}</h3>
+            //<p id="address">${brewery.city}</p>
+            //<p id="city">${brewery.city}</p>
+        
+ 
+
+
 function render() {
-    console.log(breweryInfo);
+    console.log('Brewery info: ', breweryInfo);
+    console.log('generateUI: ', generateUI());
+    $breweries.html(generateUI());
+    // return breweryInfo.map(function(brewery){
+    //     // return  $main.html( `<h3 id="name">${brewery.name}</h3>
+    //     // <p id="address">${brewery.city}</p>
+    //     // <p id="city">${brewery.city}</p>`)
+    //     console.log(brewery.name, brewery.street, brewery.city);
+    // })
+   
 }
